17e93dc2c6a33bc768f942f70a20fee716dc8007
Kunal Joshi <mail.kunaljoshi@gmail.com>
2023-04-09 02:34:11 +0530

    feat: Timer added to run periodic code

--- proxy/capture.py
+++ proxy/capture.py
@@ -1,6 +1,15 @@
 from scapy.all import *
 from packetenizer.core import CoreStructure
+from packetenizer.repeated_timer import RepeatedTimer
+from packetenizer.helper.analyzer import analyze
 
 core_structure = CoreStructure()
 
 cap = sniff(iface="eth0", prn=lambda x : core_structure.start(x), store=0)
+
+def test():
+    print("test")
+    analyzed, problem_ips = analyze(core_structure)
+    print(core_structure.serialize(analyzed, problem_ips))
+
+RepeatedTimer(5, test)
