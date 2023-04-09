c290e9c6ad88a2c3c68ceaea92537cf9833a033b
Kunal Joshi <mail.kunaljoshi@gmail.com>
2023-04-09 06:11:57 +0530

    partial: Base implementation

--- proxy/write_conf.py
+++ proxy/write_conf.py
@@ -4,20 +4,24 @@ id: unique identifier
 domains: List of string
 '''
 def write_blocklist(id, domains):
-    path = f"/etc/squid/blocked-${id}.txt"
+    # path = f"/etc/squid/blocked-${id}.txt"
+    path = f"./blocked-{id}.txt"
     f = open(path, "w+")
     f.write("\n".join(domains))
     return path
 
 def main():
-    conf_path = "/etc/squid/squid.conf"
+    # conf_path = "/etc/squid/squid.conf"
+    conf_path = "./squid.conf"
     block_list_path = write_blocklist("123", ["google.com", "facebook.com", "youtube.com"])
     src_ip = "10.0.0.0"
     f = open(conf_path, "a+")
     f.write(
         f'''
-        acl blocklist src {src_ip}    
-        acl block_domains dstdomain "{block_list_path}"
+acl blocklist src {src_ip}    
+acl block_domains dstdomain "{block_list_path}"
         '''
     )
+
+main()
     
